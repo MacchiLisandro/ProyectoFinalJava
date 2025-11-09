9796d71c269c8449f1f155c25c02b79de089e5a9
MacchiLisandro <134971163+MacchiLisandro@users.noreply.github.com>
2025-11-09 19:20:48 -0300

    Constructor Clase Persona Getters Setters
    Atributos de la clase persona

--- ProyectoFinalJava/src/Models/Persona.java
+++ ProyectoFinalJava/src/Models/Persona.java
@@ -7,5 +7,72 @@ public class Persona {
     private int telefono;
     private String email;
 
-    
+
+
+    /// Constructores
+
+
+    public Persona(String nombre, String apellido, int dni, int telefono, String email) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.dni = dni;
+        this.telefono = telefono;
+        this.email = email;
+    }
+
+    /// Getter Setter
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public int getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(int telefono) {
+        this.telefono = telefono;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+
+    /// Metodos
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Persona{" +
+                "nombre='" + nombre + '\'' +
+                ", apellido='" + apellido + '\'' +
+                ", dni=" + dni +
+                ", telefono=" + telefono +
+                ", email='" + email + '\'' +
+                '}';
+    }
 }
