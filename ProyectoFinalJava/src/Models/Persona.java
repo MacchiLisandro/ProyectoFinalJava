70e3be346df9182fadacd87f660b1cc180f7d0f8
MacchiLisandro <134971163+MacchiLisandro@users.noreply.github.com>
2025-11-09 19:18:04 -0300

    Atributos
    Atributos de la clase persona

---
+++ ProyectoFinalJava/src/Models/Persona.java
@@ -0,0 +1,11 @@
+package Models;
+
+public class Persona {
+    private String nombre;
+    private String apellido;
+    private int dni;
+    private int telefono;
+    private String email;
+
+    
+}
