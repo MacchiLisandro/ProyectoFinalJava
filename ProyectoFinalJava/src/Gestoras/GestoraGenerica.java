706b61ff595f633fd09f929819d2a8b1eddd58ac
MacchiLisandro <140757216+Mbenegasp@users.noreply.github.com>
2025-11-17 21:29:16 -0300

    branch mateo
    asd

--- ProyectoFinalJava/src/Gestoras/GestoraGenerica.java
+++ ProyectoFinalJava/src/Gestoras/GestoraGenerica.java
@@ -9,34 +9,21 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class GestoraGenerica<T extends IJson> implements IJson {
+public class GestoraGenerica<T extends IJson> {
 
     HashSet<T>contenedor=new HashSet<>();
 
 
-    @Override
-    public JSONObject toJson() {
-
-        JSONArray arrayJson=new JSONArray();
-
-        for (T elemento: contenedor){
-            arrayJson.put(elemento.toJson());
-        }
-        
-        return arrayJson;
-
-    }
-
     public void toJsonArray () throws JSONException {
         JSONArray jsonArray = new JSONArray();
         try{
-            for (T t: ejercito){
+            for (T t: contenedor){
                 jsonArray.put(t.toJson());
             }
         } catch (JSONException e){
             e.printStackTrace();
         }
-        JsonUtiles.grabarUnJson(jsonArray, "ejercito.json");
+        JsonUtiles.grabarUnJson(jsonArray, "algo.json");
     }
 
 }
