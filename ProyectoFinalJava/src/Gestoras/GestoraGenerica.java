7dd142bd17c5d6ff9b4fb986ce95d64e8c93cecc
MacchiLisandro <140757216+Mbenegasp@users.noreply.github.com>
2025-11-17 21:45:33 -0300

    update gestora
    as

--- ProyectoFinalJava/src/Gestoras/GestoraGenerica.java
+++ ProyectoFinalJava/src/Gestoras/GestoraGenerica.java
@@ -1,6 +1,7 @@
 package Gestoras;
 
 import Interfaces.IJson;
+import Models.JsonUtiles;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
@@ -8,12 +9,21 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.HashSet;
 
-public class GestoraGenerica<T extends IJson> implements IJson {
+public class GestoraGenerica<T extends IJson> {
 
     HashSet<T>contenedor=new HashSet<>();
 
 
-    @Override
-    public JSONObject toJson() throws JSONException {}
+    public void toJsonArray () throws JSONException {
+        JSONArray jsonArray = new JSONArray();
+        try{
+            for (T t: contenedor){
+                jsonArray.put(t.toJson());
+            }
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+        JsonUtiles.grabarUnJson(jsonArray, "algo.json");
+    }
 
 }
